7e2d78d1465d6cc03da11afa116376b5b3875c3a
rowanfr <beanfun1@gmail.com>
2025-01-15 03:25:33 -0500

    Finished component runner

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use wasmtime::component::*;
+use wasmtime::{Config, Engine, Store};
+
+// Generate bindings for the host
+wasmtime::component::bindgen!({
+    path: "../wit/example.wit",
+    world: "test"
+});
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut config = Config::new();
+    config.wasm_component_model(true);
+    let engine = Engine::new(&config)?;
+    let mut store = Store::new(&engine, ());
+    let linker = Linker::new(&engine);
+    let component = Component::from_file(&engine, "../wasm/example.wasm")?;
+    let test_component = Test::instantiate(&mut store, &component, &linker)?;
+    let result = test_component.call_respond(&mut store, "Rob")?;
+    println!("{}", result);
+    Ok(())
 }
